6f5c9893144079ba3261195a45522919c474b4a1
Kaka <kakalition@gmail.com>
2022-03-21 07:12:11 +0700

    Refactor: BreathPatternItemEntity, table name

--- data/src/main/java/com/daggery/present/data/entities/BreathPatternItemEntity.kt
+++ data/src/main/java/com/daggery/present/data/entities/BreathPatternItemEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "breathPattern")
+@Entity(tableName = "breath_pattern")
 data class BreathPatternItemEntity(
     @PrimaryKey val uuid: String,
     @ColumnInfo(name = "name") val name: String,
