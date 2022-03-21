888256e2c9d74578094866c0b82b2a85acb09d20
Kaka <kakalition@gmail.com>
2022-03-21 07:24:29 +0700

    Test: BreathPatternItemEntityMapperTest

---
+++ data/src/main/java/com/daggery/present/data/mappers/BreathPatternItemEntityMapper.kt
@@ -0,0 +1,11 @@
+package com.daggery.present.data.mappers
+
+import com.daggery.present.data.entities.BreathPatternItemEntity
+import com.daggery.present.domain.entities.BreathPatternItem
+import javax.inject.Inject
+
+internal class BreathPatternItemEntityMapper @Inject constructor() {
+    fun toBreathPatternItemEntity(value: BreathPatternItem) {}
+
+    fun toBreathPatternItem(value: BreathPatternItemEntity) {}
+}
