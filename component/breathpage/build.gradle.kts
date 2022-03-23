92b738ae9b8a3a25ab4671993936c49247db3b9c
Kaka <kakalition@gmail.com>
2022-03-23 20:03:00 +0700

    Progress: Create BreathPageFragment

--- component/breathpage/build.gradle.kts
+++ component/breathpage/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     implementation(project(Modules.domain))
     implementation(project(Modules.data))
     implementation(project(Modules.sharedassets))
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
     addFragmentKtx()
 
---
+++ component/breathpage/src/main/java/com/daggery/present/breathpage/view/BreathPageFragment.kt
@@ -0,0 +1,39 @@
+package com.daggery.present.breathpage.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.daggery.present.breathpage.R
+import com.daggery.present.breathpage.databinding.FragmentBreathPageBinding
+import com.daggery.present.breathpage.viewmodel.BreathPageViewModel
+import com.daggery.present.sharedassets.BundleKeys
+
+class BreathPageFragment : Fragment() {
+
+    private var _viewBinding: FragmentBreathPageBinding? = null
+    private val viewBinding get() = _viewBinding!!
+
+    private val viewModel: BreathPageViewModel by activityViewModels()
+
+    private var breathPatternUuid: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        breathPatternUuid = arguments?.getString(BundleKeys.BREATH_PATTERN_UUID)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _viewBinding = FragmentBreathPageBinding.inflate(inflater, container, false)
+        return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
