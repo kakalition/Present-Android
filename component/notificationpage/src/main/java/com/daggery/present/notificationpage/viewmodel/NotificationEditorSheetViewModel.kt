5b502f2f434919ac5f87b80f24d7ea30ded428cc
Kaka <kakalition@gmail.com>
2022-03-23 19:02:19 +0700

    Specs: NotificationPageViewModelTest and NotificationEditorSheetViewModel

---
+++ component/notificationpage/src/main/java/com/daggery/present/notificationpage/viewmodel/NotificationEditorSheetViewModel.kt
@@ -0,0 +1,8 @@
+package com.daggery.present.notificationpage.viewmodel
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class NotificationEditorSheetViewModel @Inject constructor() : ViewModel(){
+
+}
---
+++ component/notificationpage/src/main/java/com/daggery/present/notificationpage/viewmodel/NotificationPageViewModel.kt
@@ -0,0 +1,8 @@
+package com.daggery.present.notificationpage.viewmodel
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class NotificationPageViewModel @Inject constructor() : ViewModel(){
+
+}
