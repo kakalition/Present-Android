a8b3c4b93954b64dc60ee9f62d8db1a152b0729e
Kaka <kakalition@gmail.com>
2022-04-04 21:13:44 +0700

    Data Layer: Create FakeNotificationItemRepository

--- data/src/main/java/com/daggery/present/data/repositories/test/FakeNotificationItemRepository.kt
+++ data/src/main/java/com/daggery/present/data/repositories/test/FakeNotificationItemRepository.kt
@@ -1,38 +1,40 @@
-package com.daggery.present.data.db.test
+package com.daggery.present.data.repositories.test
 
-import com.daggery.present.data.db.INotificationItemDao
 import com.daggery.present.data.entities.NotificationItemEntity
+import com.daggery.present.domain.entities.NotificationItem
+import com.daggery.present.domain.repositories.NotificationItemRepository
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class FakeNotificationItemDao : INotificationItemDao {
+class FakeNotificationItemRepository : NotificationItemRepository {
 
     private val notificationItemFlow = MutableStateFlow(
         List(3) {
-            NotificationItemEntity("$it", "Notification $it", it, it, false)
+            NotificationItem("$it", "Notification $it", it, it, false)
         }
     )
 
-    override fun getNotificationsFlow(): StateFlow<List<NotificationItemEntity>> {
+    override fun getNotificationsFlow(): StateFlow<List<NotificationItem>> {
         return notificationItemFlow.asStateFlow()
     }
 
-    override suspend fun getNotifications(): List<NotificationItemEntity> {
+    override suspend fun getNotifications(): List<NotificationItem> {
         return notificationItemFlow.value
     }
 
-    override suspend fun getNotificationByUuid(uuid: String): NotificationItemEntity? {
+    override suspend fun getNotificationByUuid(uuid: String): NotificationItem? {
         return notificationItemFlow.value.singleOrNull { it.uuid == uuid }
     }
 
-    override suspend fun addNotification(value: NotificationItemEntity) {
+    override suspend fun addNotification(value: NotificationItem) {
         val newValue = notificationItemFlow.value.toMutableList()
         newValue.add(value)
         notificationItemFlow.emit(newValue)
     }
 
-    override suspend fun updateNotification(value: NotificationItemEntity) {
+    override suspend fun updateNotification(value: NotificationItem) {
         val newValue = notificationItemFlow.value.map {
             if(it.uuid == value.uuid) { value }
             else it
@@ -40,7 +42,7 @@ class FakeNotificationItemDao : INotificationItemDao {
         notificationItemFlow.emit(newValue)
     }
 
-    override suspend fun deleteNotification(value: NotificationItemEntity) {
+    override suspend fun deleteNotification(value: NotificationItem) {
         val newValue = notificationItemFlow.value.toMutableList()
         newValue.removeIf { it.uuid == value.uuid }
         notificationItemFlow.emit(newValue)
